280efd86fe57154301effa963d437037e60734ce
mrswalter404 <mrswalter404@yahoo.com>
2025-05-29 06:34:35 -0500

    new commit

---
+++ modules/s3-dynamodb/providers.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
+
+
+
+
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.84.0"
+#     }
+#   }
+# }
+
+
+
+
+
